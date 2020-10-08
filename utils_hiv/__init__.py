7caef28116db5cde57e0afa069eda466bb8c834c
Luc Blassel <luc.blassel@gmail.com>
2020-10-08 17:40:13 +0200

    more structure changes

---
+++ utils_hiv/__init__.py
@@ -0,0 +1,9 @@
+from .utils import data_utils
+from .utils import DRM_utils
+from .utils import learning_utils
+from .utils import metrics
+from .utils import param_utils
+
+__all__ = [
+    "data_utils", "DRM_utils", "learning_utils", "metrics", "param_utils"
+]
